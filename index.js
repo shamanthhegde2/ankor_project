2248d2a80706dff05b982b4bdf2912855c9b834d
shamanthhegde2 <shamanthhegde12@gmail.com>
2021-02-02 15:29:17 +0530

    refactor(main-content):bringing login and sign up in one page

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-// emailChecker=document.querySelector('.email');
-// passwordChecker=document.querySelector('.password');
-// emailChecker.addEventListener('focusout',function(event){
-//     console.log(event.target.textContent);
-// })
-// var validate=document.querySelector(".need-validation");
-// validate.addEventListener('submit',function(event){
-//     if(event.checkValidity()==false)
-//     {
-//         event.preventDefault();
-//         event.stopPropagation();
-//     }
-//     validate.classList.add("was-validated");
-// })
-var validate=document.querySelector(".need-validation");
-var validation=document.querySelector(".validation");
-validate.addEventListener('submit',function(event){
-    var password=document.querySelector("#password");
-    var confirmPassword=document.querySelector("#confirmPassword");
-    if(password.value!=confirmPassword.value)
-    {
-        event.preventDefault();
-        event.stopPropagation();
-        validation.innerHTML="⚠ password mismatch";
-        
-    }
-    setTimeout(function(){
-        validation.innerHTML="";
-    },1700);
+/*this code is to validate the password and the confirm password*/
+var validate = document.querySelector('.need-validation')
+var validation = document.querySelector('.validation')
+validate.addEventListener('submit', function (event) {
+  var password = document.querySelector('#password')
+  var confirmPassword = document.querySelector('#confirmPassword')
+  if (password.value != confirmPassword.value) {
+    event.preventDefault()
+    event.stopPropagation()
+    validation.innerHTML = '⚠ password mismatch'
+  }
+  setTimeout(function () {
+    validation.innerHTML = ''
+  }, 1700)
+})
+
+/*this is to show one page at a time infront*/
+var login = document.querySelector('.login')
+var register = document.querySelector('.signup')
+var login_button = document.querySelector('.login-nav')
+var reg_button = document.querySelector('.reg-nav')
+register.classList.add('display-none')
+login_button.classList.add('nav-color')
+reg_button.addEventListener('click', function () {
+  login.classList.add('display-none')
+  register.classList.remove('display-none')
+  login_button.classList.remove('nav-color')
+  reg_button.classList.add('nav-color')
+})
+login_button.addEventListener('click', function () {
+  register.classList.add('display-none')
+  login.classList.remove('display-none')
+  reg_button.classList.remove('nav-color')
+  login_button.classList.add('nav-color')
 })
